6983d05f754560443b91be6457e9a6edb2f028a8
Bashkatov-spb <bashkatov.spb@mail.ru>
2023-05-28 10:33:40 +0300

    feat: change components on features and added registration form in App

---
+++ client/src/App/App.tsx
@@ -0,0 +1,12 @@
+import RegistrationForm from '../features/auth/RegistrationForm';
+
+function App() {
+  return (
+    <>
+      <h1>Work!</h1>
+      <RegistrationForm />
+    </>
+  );
+}
+
+export default App;
