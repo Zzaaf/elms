c88257688c7e210b6903060b32c5ca7888646545
Bashkatov-spb <bashkatov.spb@mail.ru>
2023-07-06 06:31:01 +0300

    refactor: change some styles, added fonts

--- client/src/features/navbar/Navbar.tsx
+++ client/src/features/navbar/Navbar.tsx
@@ -1,46 +1,48 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import './styles/NavbarStyle.scss'
-import { RootState, useAppDispatch } from '../../store'
-import { logoutStudent } from '../students/studentSlice'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './styles/NavbarStyle.scss';
+import { RootState, useAppDispatch } from '../../store';
+import { logoutStudent } from '../students/studentSlice';
+import { useSelector } from 'react-redux';
 
 const Navbar = () => {
-  const dispatch = useAppDispatch()
-  const {student} = useSelector((store:RootState)=>store.student)
-  const logout =()=>{
-    dispatch(logoutStudent())
-  }
+  const dispatch = useAppDispatch();
+  const { student } = useSelector((store: RootState) => store.student);
+  const logout = () => {
+    dispatch(logoutStudent());
+  };
   return (
-    <div className='container'>
-      <div>
-          <NavLink to="/presentations">Презентации</NavLink>
-        </div>
-        <div>
-          <NavLink to="/videos">Видео</NavLink>
-        </div>
-      {!student ?
-      <>
-        <div>
-          <NavLink to='/auth/registr'>Регистрация</NavLink>
-        </div>
-        <div>
-          <NavLink to='/auth/login/student'>Авторизация</NavLink>
-        </div>
-       </>
-      :
-      <>
-        <div>
-          <NavLink to='/students'>Студенты</NavLink>
-        </div>
-        <div>
-          <NavLink to='/student/profile'>Профиль</NavLink>
-        </div>
-        <div>
-          <NavLink to='' onClick={logout}>Logout</NavLink>
-        </div>
-      </>
-      }
+    <div className="container">
+      {!student ? (
+        <>
+          <div>
+            <NavLink to="/auth/registration">Регистрация</NavLink>
+          </div>
+          <div>
+            <NavLink to="/auth/authorization">Авторизация</NavLink>
+          </div>
+        </>
+      ) : (
+        <>
+          <div>
+            <NavLink to="/presentations">Презентации</NavLink>
+          </div>
+          <div>
+            <NavLink to="/videos">Видео</NavLink>
+          </div>
+          <div>
+            <NavLink to="/students">Студенты</NavLink>
+          </div>
+          <div>
+            <NavLink to="/student/profile">Профиль</NavLink>
+          </div>
+          <div>
+            <NavLink to="" onClick={logout}>
+              Logout
+            </NavLink>
+          </div>
+        </>
+      )}
     </div>
   );
 };
