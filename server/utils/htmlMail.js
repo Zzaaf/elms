aa70546b346738a87dbaf96e23d431e3ba91fd54
AnnaMVSPB <makarobaanya@yandex.ru>
2023-07-26 10:56:08 +0300

    fix: fixed register form and mail

---
+++ server/utils/htmlMail.js
@@ -0,0 +1,159 @@
+function html(authenticationUrl) {
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title></title>
+  </head>
+  <body>
+  <div class="es-wrapper-color" >
+      <!--[if gte mso 9]>
+          <v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t">
+              <v:fill type="tile" color="#f8f9fd"></v:fill>
+          </v:background>
+      <![endif]-->
+      <table class="es-wrapper" width="100%" cellspacing="0" cellpadding="0" style="background-color: #f8f9fd;">
+          <tbody>
+              <tr>
+                  <td class="esd-email-paddings" valign="top">
+                      <table cellpadding="0" cellspacing="0" class="esd-header-popover es-header" align="center">
+                          <tbody>
+                              <tr>
+                                  <td class="esd-stripe" align="center">
+                                      <table  class="es-header-body" align="center" cellpadding="0" cellspacing="0" width="600">
+                                          <tbody>
+                                              <tr>
+                                                  <td class="esd-structure es-p10t es-p15b es-p30r es-p30l" align="left">
+                                                      <table cellpadding="0" cellspacing="0" width="100%">
+                                                          <tbody>
+                                                              <tr>
+                                                                  <td width="600" class="esd-container-frame" align="center" valign="top">
+                                                                      <table cellpadding="0" cellspacing="0" width="100%">
+                                                                          <tbody>
+                                                                              <tr>
+                                                                                  <td align="center" class="esd-block-image" style="font-size: 0px;"><a target="_blank"><img src="https://media.proglib.io/events/2020/06/05/139630d138c5138dcc0f1dfaf06b3846.jpg" alt style="display: block;" width="600"></a></td>
+                                                                              </tr>
+                                                                          </tbody>
+                                                                      </table>
+                                                                  </td>
+                                                              </tr>
+                                                          </tbody>
+                                                      </table>
+                                                  </td>
+                                              </tr>
+                                          </tbody>
+                                      </table>
+                                  </td>
+                              </tr>
+                          </tbody>
+                      </table>
+                      <table cellpadding="0" cellspacing="0" class="es-content" align="center">
+                          <tbody>
+                              <tr>
+                                  <td class="esd-stripe" align="center" bgcolor="#f8f9fd" style="background-color: #f8f9fd;">
+                                      <table bgcolor="transparent" class="es-content-body" align="center" cellpadding="0" cellspacing="0" width="600" style="background-color: transparent;">
+                                          <tbody>
+                                              <tr>
+                                                  <td class="esd-structure es-p20t es-p10b es-p20r es-p20l" align="left">
+                                                      <table cellpadding="0" cellspacing="0" width="100%">
+                                                          <tbody>
+                                                              <tr>
+                                                                  <td width="600" class="esd-container-frame" align="center" valign="top">
+                                                                      <table cellpadding="0" cellspacing="0" >
+                                                                          <tbody>
+                                                                              <tr>
+                                                                                  <td align="center" class="esd-block-text es-p10b">
+                                                                                      <h1>Привет, друг 👋</h1>
+                                                                                  </td>
+                                                                              </tr>
+                                                                          </tbody>
+                                                                      </table>
+                                                                  </td>
+                                                              </tr>
+                                                          </tbody>
+                                                      </table>
+                                                  </td>
+                                              </tr>
+                                          </tbody>
+                                      </table>
+                                  </td>
+                              </tr>
+                          </tbody>
+                      </table>
+                      <table cellpadding="0" cellspacing="0" class="es-content" align="center">
+                          <tbody>
+                              <tr>
+                                  <td class="esd-stripe" align="center">
+                                      <table  class="es-content-body" align="center" cellpadding="0" cellspacing="0" width="600">
+                                          <tbody>
+                                              <tr>
+                                                  <td class="es-p20t es-p20r es-p20l esd-structure" align="left">
+                                                      <table cellpadding="0" cellspacing="0" width="100%">
+                                                          <tbody>
+                                                              <tr>
+                                                                  <td width="600" class="esd-container-frame" align="center" valign="top">
+                                                                      <table cellpadding="0" cellspacing="0" width="100%">
+                                                                          <tbody>
+                                                                              <tr>
+                                                                                  <td align="center" class="esd-block-text">
+                                                                                      <p> Для завершения регистрации перейди по ссылке ниже 👇</p>
+                                                                                  </td>
+                                                                              </tr>
+                                                                          </tbody>
+                                                                      </table>
+                                                                  </td>
+                                                              </tr>
+                                                          </tbody>
+                                                      </table>
+                                                  </td>
+                                              </tr>
+                                          </tbody>
+                                      </table>
+                                  </td>
+                              </tr>
+                          </tbody>
+                      </table>
+                      <table cellpadding="0" cellspacing="0" class="es-content esd-footer-popover" align="center">
+                          <tbody>
+                              <tr>
+                                  <td class="esd-stripe" align="center">
+                                      <table class="es-content-body" align="center" cellpadding="0" cellspacing="0" width="600">
+                                          <tbody>
+                                              <tr>
+                                                  <td class="es-p20t es-p20r es-p20l esd-structure" align="left">
+                                                      <table cellpadding="0" cellspacing="0" width="100%">
+                                                          <tbody>
+                                                              <tr>
+                                                                  <td width="600" class="esd-container-frame" align="center" valign="top">
+                                                                      <table cellpadding="0" cellspacing="0" width="100%">
+                                                                          <tbody>
+                                                                              <tr >
+                                                                                  <td align="center" ><button class="es-button-border-1690273787476 es-button-border" style="background: #29edff;border-radius: 30px; height:50px; width: 200px;"><a href="https://localhost:5173/auth/authorization/${authenticationUrl}" class="es-button es-button-1690273787462" target="_blank" style="background: #29edff; color: #4520ab; text-decoration: none; ">Вернуться на сайт</a></button></td>
+                                                                              </tr>
+                                                                          </tbody>
+                                                                      </table>
+                                                                  </td>
+                                                              </tr>
+                                                          </tbody>
+                                                      </table>
+                                                  </td>
+                                              </tr>
+                                          </tbody>
+                                      </table>
+                                  </td>
+                              </tr>
+                          </tbody>
+                      </table>
+                  </td>
+              </tr>
+          </tbody>
+      </table>
+  </div>
+  </body>
+  </html>
+  `;
+}
+module.exports = html;
